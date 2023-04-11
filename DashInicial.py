9fa231eca6a114a657198d5af6d4c9d65b371fab
Daniel Messias da Silva Pinto <82840303+danielmpinto@users.noreply.github.com>
2023-04-11 18:36:50 -0400

    Add files via upload

---
+++ DashInicial.py
@@ -0,0 +1,42 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Maçã", "Laranja", "Banana", "Maçã", "Laranja", "Banana"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+    dcc.Markdown(
+       '''
+            # Olá Mundo, sou eu!
+       '''
+       ),         
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    ),
+  dcc.Dropdown(['NYC', 'MTL', 'SF'], value='NYC', id='demo-dropdown'),
+    html.Div(id='dd-output-container')           
+])
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0',debug=True,port=5010)
+
